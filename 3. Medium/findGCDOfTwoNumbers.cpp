513fb9289e4ef0a9bb4e98209ab2cce3bcfd68b7
Rajarshi Sengupta <55212405+rajarshisg@users.noreply.github.com>
2020-10-03 18:44:27 +0530

    Create findGCDOfTwoNumbers.cpp

---
+++ 3. Medium/findGCDOfTwoNumbers.cpp
@@ -0,0 +1,13 @@
+/* Problem - GCD and LCM ( Part 1)
+   Problem Statement - Create a function that takes two numbers as arguments and returns the Greatest Common Devisor (GCD) of the two numbers.
+   Problem Link - https://edabit.com/challenge/BzEQsTSmPATbsZJCR
+*/
+int gcd(int a, int b) {
+	int min=(a<b)?a:b;
+	int gcd=1;
+	for(int i=1;i<=min;i++){
+		if(a%i==0 && b%i==0)
+			gcd=i;
+	}
+	return gcd;
+}
